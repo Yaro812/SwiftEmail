cdebcfbb747b9ce16e6a9ca05172f2d59a1f919a
Thorax <thorax@me.com>
2018-09-25 20:14:31 +0300

    Initial Commit

---
+++ SwiftEmail/SwiftEmail.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEmail.h
+//  SwiftEmail
+//
+//  Created by Yaroslav Pasternak on 25/09/2018.
+//  Copyright © 2018 Thorax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftEmail.
+FOUNDATION_EXPORT double SwiftEmailVersionNumber;
+
+//! Project version string for SwiftEmail.
+FOUNDATION_EXPORT const unsigned char SwiftEmailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEmail/PublicHeader.h>
+
+
